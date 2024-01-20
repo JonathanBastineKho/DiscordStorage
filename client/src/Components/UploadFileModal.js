5e1295e677dcd3ce4002ed9f0a35e605b5e605a4
edkesuma <edrickkesuma21@gmail.com>
2024-01-21 02:32:05 +0800

    wip

--- client/src/Components/UploadFileModal.js
+++ client/src/Components/UploadFileModal.js
@@ -1,11 +1,44 @@
-import { Button, Label, Modal, TextInput } from "flowbite-react";
+import { Button, Label, Modal, Progress, TextInput } from "flowbite-react";
 import React from "react";
 import { useState } from "react";
 import DropImageInput from "./DropImageInput";
+import axios from "axios";
 
 function UploadFileModal(props) {
   const show = true;
   const [file, setFile] = useState(null);
+
+  const [progress, setProgress] = useState(0);
+  const [visible, setVisible] = useState(false);
+
+  const uploadFile = (token, file, folderId) => {
+
+    const chunkSize = 25 * 1024 * 1024;
+    const totalChunks = Math.ceil(file.size / chunkSize);
+
+    for (let i = 0; i < totalChunks; i++) {
+      var formData = new FormData();
+      formData.append(file);
+      formData.append(i);
+      formData.append(file.name);
+      formData.append(folderId);
+      formData.append(file.size);
+      if (token) {
+        axios
+          .post("https://localhost:8000/api/upload", formData, {
+            headers: {
+              Authorization: `${token}`,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((res) => {
+            if (res.data.success) {
+              setProgress(i / totalChunks);
+            }
+          });
+      }
+    }
+  };
 
   return (
     <Modal
@@ -22,6 +55,20 @@ function UploadFileModal(props) {
             <TextInput id="fileName" type="email" placeholder required />
           </span>
           <DropImageInput show={true} file={file} setFile={setFile} />
+          {visible && (
+            <Progress
+              progress={progress}
+              progressLabelPosition="inside"
+              textLabel="Uploading..."
+              textLabelPosition="outside"
+              size="md"
+              labelProgress
+              labelText
+            />
+          )}
+          <Button className="rounded-lg" onClick={console.log("yep")}>
+            Upload File
+          </Button>
         </div>
       </Modal.Body>
     </Modal>
