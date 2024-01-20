7d5d16fcbc71f602203fe7ad46dd9a099870c251
Jonathan Bastine Kho <jonathanbastine@gmail.com>
2024-01-21 05:37:38 +0800

    Update PathBreadCrumb.js

--- client/src/Components/PathBreadCrumb.js
+++ client/src/Components/PathBreadCrumb.js
@@ -2,12 +2,7 @@ import React from "react";
 import { Breadcrumb, Label } from "flowbite-react";
 
 function PathBreadCrumb(props) {
-  const listPath = [
-    { folderId: 1, folderName: "aaaaaa" },
-    { folderId: 2, folderName: "bbbbbb" },
-    { folderId: 3, folderName: "cccccc" },
-  ];
-  console.log(props.listPath);
+
   return (
     <div className="mb-4">
       <Breadcrumb className="py-3">
